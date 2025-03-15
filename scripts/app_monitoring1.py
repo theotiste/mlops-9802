52d036e746a04d87b364ded225c362dd4d9c8b48
theotiste <62854280+theotiste@users.noreply.github.com>
2025-03-15 20:43:35 +0100

    Update app_monitoring1.py

--- scripts/app_monitoring1.py
+++ scripts/app_monitoring1.py
@@ -4,16 +4,18 @@ import pandas as pd
 
 app = Flask(__name__)
 model = pickle.load(open("catboost_model-1.pkl", "rb"))
-                                        
+
 
 def model_pred(features):
     test_data = pd.DataFrame([features])
     prediction = model.predict(test_data)
     return int(prediction[0])
 
+
 @app.route("/", methods=["GET"])
-def Home():
+def home():
     return render_template("index.html")
+
 
 @app.route("/predict", methods=["POST"])
 def predict():
@@ -25,21 +27,26 @@ def predict():
         Income = float(request.form["Income"])
         Years_employed = int(request.form["Years_employed"])
         Fico_score = int(request.form["Fico_score"])
-        
+
         prediction = model.predict(
-            [[Customer_id, Credit_line_outstanding, Loan_amt_outstanding, Total_debt_outstanding, Income, Years_employed, Fico_score]]
+            [[
+                Customer_id,
+                Credit_line_outstanding,
+                Loan_amt_outstanding,
+                Total_debt_outstanding,
+                Income,
+                Years_employed,
+                Fico_score,
+            ]]
         )
-        
+
         if prediction[0] == 1:
             return render_template(
-                "index.html",
-                prediction_text="Make an appointment with your banker",
+                "index.html", prediction_text="Make an appointment with your banker"
             )
         else:
-            return render_template(
-                "index.html",
-                prediction_text="loan granted"
-            )
+            return render_template("index.html", prediction_text="loan granted")
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)      
+    app.run(host="0.0.0.0", port=5000, debug=True)
